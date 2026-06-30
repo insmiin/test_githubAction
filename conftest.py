b695ee803cf64800aa356269277eb68f969e179c
miinMorpheus <lim.miin@MorpheusAD.local>
2026-06-30 23:14:25 +0800

    add4

--- conftest.py
+++ conftest.py
@@ -4,25 +4,24 @@ from mysql.connector import Error
 
 @pytest.fixture(scope="session")
 def my_conftest_test():
-    @pytest.fixture(scope="session")
-    def mysql_connection():
-        print('mysql_connection fixtureee')
-        conn = None
-        try:
-            conn = mysql.connector.connect(
-                host='35.229.171.142',
-                user='sfqat_superadmin_qa',  # 'qat_readonly',
-                password='dL72QF1Ia4',  # 'NTZ6Wt3tzqCp4k7v',
-                port=3306,
-                database='GB_Qat'
-                # !!this is DATABASEname(GB_Qat) in mysql , not the connectionName on mysql workbench....
-            )
-            yield conn
-        except Error as err:
-            pytest.fail(f"MySQL connection error: {err}")
-        finally:
-            print('mysql at finally')
-            if conn and conn.is_connected():
-                print('going to close the mysql connection')
-                conn.close()
+    print('mysql_connection fixtureee')
+    conn = None
+    try:
+        conn = mysql.connector.connect(
+            host='35.229.171.142',
+            user='sfqat_superadmin_qa',  # 'qat_readonly',
+            password='dL72QF1Ia4',  # 'NTZ6Wt3tzqCp4k7v',
+            port=3306,
+            database='GB_Qat'
+            # !!this is DATABASEname(GB_Qat) in mysql , not the connectionName on mysql workbench....
+        )
+        yield conn
+    except Error as err:
+        pytest.fail(f"MySQL connection error: {err}")
+    finally:
+        print('mysql at finally')
+        if conn and conn.is_connected():
+            print('going to close the mysql connection')
+            conn.close()
+
     return 'abc'
